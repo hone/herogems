92af2a8e2bd9fd2b14d495c68204df1ceb45cc8b
Terence Lee <hone02@gmail.com>
2011-05-01 19:32:31 -0500

    don't need to run these tests anymore

--- spec/lib/herogems_spec.rb
+++ spec/lib/herogems_spec.rb
@@ -2,12 +2,12 @@ require File.join(File.dirname(__FILE__), '../spec_helper')
 
 describe "Herogems" do
   describe "test framework" do
-    it "should not blow up" do
+    xit "should not blow up" do
       run("help db", :expect_error => true)
       @err.should be_empty
     end
 
-    it "should install the gem" do
+    xit "should install the gem" do
       # need to ignore for now, need to find a non broken rubygems
       `gem install #{plugin_gem_path('herobro')} --ignore-dependencies`
       `gem list`.should include("herobro")
